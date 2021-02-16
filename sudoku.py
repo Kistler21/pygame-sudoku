2a501c421bac86408f4b51b1056f0eabe4e4873c
Michael Kistler <michaelkis213@yahoo.com>
2021-02-16 13:22:30 -0500

    Add Solve and Reset button elements
    A Solve button and a Reset button were added to the game. Currently,
    clicking either button has no functionality. The buttons change to
    a darker shade when the mouse hovers over them.

--- sudoku.py
+++ sudoku.py
@@ -9,15 +9,21 @@ pygame.init()
 cell_size = 50
 minor_grid_size = 1
 major_grid_size = 3
-edge_buffer = 5
-width = cell_size*9 + minor_grid_size*6 + major_grid_size*4 + edge_buffer*2
-height = cell_size*9 + minor_grid_size*6 + major_grid_size*4 + edge_buffer*2
+buffer = 5
+button_height = 50
+button_width = 100
+button_border = 2
+width = cell_size*9 + minor_grid_size*6 + major_grid_size*4 + buffer*2
+height = cell_size*9 + minor_grid_size*6 + \
+    major_grid_size*4 + button_height + buffer*3 + button_border*2
 size = width, height
 white = 255, 255, 255
 black = 0, 0, 0
 gray = 200, 200, 200
 green = 0, 175, 0
 red = 200, 0, 0
+inactive_btn = 51, 255, 255
+active_btn = 51, 153, 255
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Sudoku')
@@ -42,8 +48,8 @@ def create_cells():
     # Set attributes for for first RectCell
     row = 0
     col = 0
-    left = edge_buffer + major_grid_size
-    top = edge_buffer + major_grid_size
+    left = buffer + major_grid_size
+    top = buffer + major_grid_size
 
     while row < 9:
         while col < 9:
@@ -59,7 +65,7 @@ def create_cells():
         top += cell_size + minor_grid_size
         if row != 0 and (row + 1) % 3 == 0:
             top = top + major_grid_size - minor_grid_size
-        left = edge_buffer + major_grid_size
+        left = buffer + major_grid_size
         col = 0
         row += 1
 
@@ -70,12 +76,12 @@ def draw_grid():
     '''Draws the major and minor grid lines for Sudoku.'''
     # Draw minor grid lines
     lines_drawn = 0
-    pos = edge_buffer + major_grid_size + cell_size
+    pos = buffer + major_grid_size + cell_size
     while lines_drawn < 6:
-        pygame.draw.line(screen, black, (pos, edge_buffer),
-                         (pos, width-edge_buffer-1), minor_grid_size)
-        pygame.draw.line(screen, black, (edge_buffer, pos),
-                         (width-edge_buffer-1, pos), minor_grid_size)
+        pygame.draw.line(screen, black, (pos, buffer),
+                         (pos, width-buffer-1), minor_grid_size)
+        pygame.draw.line(screen, black, (buffer, pos),
+                         (width-buffer-1, pos), minor_grid_size)
 
         # Update number of lines drawn
         lines_drawn += 1
@@ -86,11 +92,11 @@ def draw_grid():
             pos += cell_size + major_grid_size
 
     # Draw major grid lines
-    for pos in range(edge_buffer+major_grid_size//2, width, cell_size*3 + minor_grid_size*2 + major_grid_size):
-        pygame.draw.line(screen, black, (pos, edge_buffer),
-                         (pos, width-edge_buffer-1), major_grid_size)
-        pygame.draw.line(screen, black, (edge_buffer, pos),
-                         (width-edge_buffer-1, pos), major_grid_size)
+    for pos in range(buffer+major_grid_size//2, width, cell_size*3 + minor_grid_size*2 + major_grid_size):
+        pygame.draw.line(screen, black, (pos, buffer),
+                         (pos, width-buffer-1), major_grid_size)
+        pygame.draw.line(screen, black, (buffer, pos),
+                         (width-buffer-1, pos), major_grid_size)
 
 
 def fill_cells(cells, board):
@@ -124,6 +130,34 @@ def fill_cells(cells, board):
             screen.blit(text, textbox)
 
 
+def draw_button(left, top, width, height, border, color, border_color, text):
+    '''Creates a button with a border.'''
+    # Draw the border as outer rect
+    pygame.draw.rect(
+        screen,
+        border_color,
+        (left, top, width+border*2, height+border*2),
+    )
+
+    # Draw the inner button
+    button = pygame.Rect(
+        left+border,
+        top+border,
+        width,
+        height
+    )
+    pygame.draw.rect(screen, color, button)
+
+    # Set the text
+    font = pygame.font.Font(None, 26)
+    text = font.render(text, 1, black)
+    xpos, ypos = button.center
+    textbox = text.get_rect(center=(xpos, ypos))
+    screen.blit(text, textbox)
+
+    return button
+
+
 def play():
     '''Contains all the functionality for playing a game of Sudoku.'''
     easy = [
@@ -150,7 +184,7 @@ def play():
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_pos = pygame.mouse.get_pos()
 
-                # Test if point in any rect
+                # Test if point in any cell
                 active_cell = None
                 for row in cells:
                     for cell in row:
@@ -158,7 +192,7 @@ def play():
                             active_cell = cell
 
                 # Test if active cell is empty
-                if not game.board[active_cell.row][active_cell.col].editable:
+                if active_cell and not game.board[active_cell.row][active_cell.col].editable:
                     active_cell = None
 
             # Handle key press
@@ -190,10 +224,62 @@ def play():
                         game.board[active_cell.row][active_cell.col].value = None
 
         screen.fill(white)
+
+        # Draw grid and cells
         draw_grid()
         if active_cell is not None:
             pygame.draw.rect(screen, gray, active_cell)
+
+        # Create buttons
+        reset_btn = draw_button(
+            width - buffer - button_border*2 - button_width,
+            height - button_height - button_border*2 - buffer,
+            button_width,
+            button_height,
+            button_border,
+            inactive_btn,
+            black,
+            'Reset'
+        )
+        solve_btn = draw_button(
+            width - buffer*2 - button_border*4 - button_width*2,
+            height - button_height - button_border*2 - buffer,
+            button_width,
+            button_height,
+            button_border,
+            inactive_btn,
+            black,
+            'Solve'
+        )
+
+        # Check if mouse over either button
+        if reset_btn.collidepoint(pygame.mouse.get_pos()):
+            reset_btn = draw_button(
+                width - buffer - button_border*2 - button_width,
+                height - button_height - button_border*2 - buffer,
+                button_width,
+                button_height,
+                button_border,
+                active_btn,
+                black,
+                'Reset'
+            )
+        if solve_btn.collidepoint(pygame.mouse.get_pos()):
+            solve_btn = draw_button(
+                width - buffer*2 - button_border*4 - button_width*2,
+                height - button_height - button_border*2 - buffer,
+                button_width,
+                button_height,
+                button_border,
+                active_btn,
+                black,
+                'Solve'
+            )
+
+        # Fill in cell values
         fill_cells(cells, game)
+
+        # Update screen
         pygame.display.flip()
 
 
